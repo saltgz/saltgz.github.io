9a508323549b27e335fd3e77975ccabe597ea27b
szingaro <stefanopio.zingaro@unibo.it>
2019-02-17 21:15:31 +0100

    color primary just to main content links

--- assets/js/minimal-bootstrap.js
+++ assets/js/minimal-bootstrap.js
@@ -1,6 +1,8 @@
 $(function() {
   $("p").addClass("text-body");
-  $("a").addClass("text-info");
+  $("header").find("a").addClass("text-dark");
+  $("main").find("a").addClass("text-primary");
+  $("i").addClass("fa-2x");
   $("table").addClass("table table-striped text-left");
   $("a").click(function() {
     var a = new RegExp("/" + window.location.host + "/");

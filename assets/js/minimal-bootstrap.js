c25499c1faee1a7a79d27d4b9e3a8b907e88fb74
szingaro <28324421+szingaro@users.noreply.github.com>
2019-02-18 23:45:02 +0100

    Update minimal-bootstrap.js

--- assets/js/minimal-bootstrap.js
+++ assets/js/minimal-bootstrap.js
@@ -2,7 +2,7 @@ $(function() {
   $("p").addClass("text-body");
   $("header").find("a").addClass("text-dark");
   $("main").find("a").addClass("text-primary");
-  $("i").addClass("fa-2x text-danger");
+  $("i").addClass("text-dark");
   $("table").addClass("table table-striped text-left");
   $("a").click(function() {
     var a = new RegExp("/" + window.location.host + "/");

8198116806a190cc7c19b6d495252835f4b1f3fc
szingaro <stefanopio.zingaro@unibo.it>
2019-03-29 12:39:26 +0100

    fixing menu and tabels bug responsiveness

--- assets/js/minimal-bootstrap.js
+++ assets/js/minimal-bootstrap.js
@@ -2,7 +2,8 @@ $(function() {
   $("p").addClass("text-body");
   $("header").find("a").addClass("text-dark");
   $("main").find("a").addClass("text-primary");
-  $("i").addClass("text-success");
+  $("i").addClass("text-warning");
+  $("table").wrap("<div class=\"table-responsive\" />");
   $("table").addClass("table table-striped table-dark text-left");
   $("a").click(function() {
     var a = new RegExp("/" + window.location.host + "/");
@@ -10,4 +11,5 @@ $(function() {
       $(this).attr("target", "_blank");
     }
   });
+
 });

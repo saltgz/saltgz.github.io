cf8cb473500636ecc3c0c1b78b93e0d4aa12d5ed
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2019-09-24 20:59:38 +0200

    twitter dinamic height fixed

--- assets/js/minimal-bootstrap.js
+++ assets/js/minimal-bootstrap.js
@@ -1,15 +1,16 @@
-$(function() {
+$(function () {
+  // $("#twitter-widget-0").height($("#center-column").clientHeight);
   $("p").addClass("text-body");
   $("header").find("a").addClass("text-dark");
   $("main").find("a").addClass("text-primary");
   $("i").addClass("text-warning");
   $("table").wrap("<div class=\"table-responsive\" />");
   $("table").addClass("table table-striped text-left");
-  $("a").click(function() {
+  $("a").click(function () {
     var a = new RegExp("/" + window.location.host + "/");
     if (!a.test(this.href)) {
       $(this).attr("target", "_blank");
     }
   });
-
+  $(".twitter-timeline").attr("data-height", $("#center-column").height())
 });
